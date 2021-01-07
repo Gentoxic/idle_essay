2760abbed42bb6f84377f777a443db5ecc586bfb
Kevin Shao <kevin2002@hotmail.ca>
2021-01-06 20:47:19 -0700

    created files and started coding

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,26 @@
+// Title: Idle Essay
+// Creator: Kevin Shao
+// Date created: 1/6/2021
+
+// Program description: You are supposed to write essays.
+
+fun main(){
+    println("hello")
+    val player = Player()
+    println(player.name)
+    player.change_name()
+    println(player.name)
+}
+
+class Player{
+    // properties
+    var name = 4
+    var click = 1 // how many words are written when the screen is clicked
+    var upgrade = 0 // Different upgrades for the player
+
+
+    fun change_name(){
+        var name = 6
+    }
+
+}
